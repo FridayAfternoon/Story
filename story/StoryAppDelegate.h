775c833806e13dcc5d636d3f854d0fe8a1b1aa58
FridayAfternoon <wuxutao@gmail.com>
2012-11-13 07:06:30 +0800

    my story

---
+++ story/StoryAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StoryAppDelegate.h
+//  story
+//
+//  Created by wuxt on 12-11-13.
+//  Copyright (c) 2012年 wuxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ story/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  story
+//
+//  Created by wuxt on 12-11-13.
+//  Copyright (c) 2012年 wuxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StoryAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StoryAppDelegate class]));
+    }
+}
